8c35fd70c154c86d5664515216f314a331c9c537
rehmanm <muhammad.abdulrehman@gmail.com>
2023-08-08 23:40:46 -0400

    database insert completed

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,11 +8,13 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/rehmanm/snippetbox/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -48,6 +50,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
